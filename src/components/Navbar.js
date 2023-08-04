aff5a221f655e6dc7ff3a498debd3b4f1c79bd54
suraj <sysnicpro123@gmail.com>
2023-08-04 18:39:15 +0530

    tutorial 25

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+              NewsMonkey
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarScroll"
+              aria-controls="navbarScroll"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarScroll">
+              <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
+                    Home
+                  </a>
+                </li>
+
+                <li className="nav-item">
+                  <a
+                    className="nav-link disabled"
+                    aria-disabled="true"
+                    href="/about"
+                  >
+                    About
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </>
+    );
+  }
+}
+
+export default Navbar;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+export class NewsItem extends Component {
+  render() {
+    //using destructuring to get props from the class object
+    let { title, description, imgUrl, newsUrl } = this.props;
+
+    return (
+      <div>
+        <div className="card" style={{ width: '18rem' }}>
+          <img src={imgUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
